a667d2ddb62f508852204a76dbfcece79213c0d5
Atul Kumar Dubey <152834462+Atuld-123@users.noreply.github.com>
2025-07-17 15:05:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,3 +0,0 @@
-
-function toggleMenu(){document.getElementById('nav-menu').classList.toggle('active');}
-const topBtn=document.getElementById('back-to-top');window.addEventListener('scroll',()=>{topBtn.style.display=window.scrollY>300?'block':'none';});topBtn.addEventListener('click',()=>window.scrollTo({top:0,behavior:'smooth'}));
